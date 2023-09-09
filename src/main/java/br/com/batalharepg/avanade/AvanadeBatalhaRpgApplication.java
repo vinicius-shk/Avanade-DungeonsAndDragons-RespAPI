2711cda702b6ebc7afa20639eba391734c1d50ad
vinicius.shk <vinicius.shk@gmail.com>
2023-09-09 09:20:28 -0300

    Criacao do projeto
    Signed-off-by: vinicius.shk <vinicius.shk@gmail.com>

---
+++ src/main/java/br/com/batalharepg/avanade/AvanadeBatalhaRpgApplication.java
@@ -0,0 +1,13 @@
+package br.com.batalharepg.avanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeBatalhaRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeBatalhaRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/batalharepg/avanade/AvanadeBatalhaRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batalharepg.avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeBatalhaRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
