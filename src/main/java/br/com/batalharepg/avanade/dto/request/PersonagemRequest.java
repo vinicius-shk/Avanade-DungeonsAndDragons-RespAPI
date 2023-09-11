ab9560b003cdf20d13d1516470963031687cb56c
vinicius.shk <vinicius.shk@gmail.com>
2023-09-11 17:09:44 -0300

    feat(Dto): Request
    Implementa validacao de dados de request com handler de erros

--- src/main/java/br/com/batalharepg/avanade/dto/request/PersonagemRequest.java
+++ src/main/java/br/com/batalharepg/avanade/dto/request/PersonagemRequest.java
@@ -1,7 +1,10 @@
 package br.com.batalharepg.avanade.dto.request;
 
 import br.com.batalharepg.avanade.factory.personagem.TipoClassePersonagem;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record PersonagemRequest(String nome,
+public record PersonagemRequest(@NotNull(message = "Nome deve ser informado") @Size(min = 2, max = 20, message = "Nome deve ter entre 2 e 20 caracteres") String nome,
+                                @NotNull(message = "Tipo de classe deve ser informado")
                                 TipoClassePersonagem tipoClassePersonagem) {
 }
