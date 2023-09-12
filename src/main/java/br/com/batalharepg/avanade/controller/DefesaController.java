bc06b61e602352c852524c52f686632a0e16d288
vinicius.shk <vinicius.shk@gmail.com>
2023-09-12 11:41:35 -0300

    refactor(Combate): DtoResponse
    Refatora dto de resposta para as acoes de ataque, defesa e dano para retornar os dados relevantes apenas ao turno atual

--- src/main/java/br/com/batalharepg/avanade/controller/DefesaController.java
+++ src/main/java/br/com/batalharepg/avanade/controller/DefesaController.java
@@ -1,6 +1,6 @@
 package br.com.batalharepg.avanade.controller;
 
-import br.com.batalharepg.avanade.dto.response.BatalhaDetalhesResponse;
+import br.com.batalharepg.avanade.dto.response.DadosTurnoResponse;
 import br.com.batalharepg.avanade.factory.combate.TipoAcao;
 import br.com.batalharepg.avanade.service.CombateService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,13 +27,13 @@ public class DefesaController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Defesa já realizada",
             content = { @Content(mediaType = "application/json",
-                schema = @Schema(implementation = BatalhaDetalhesResponse.class)) }),
+                schema = @Schema(implementation = DadosTurnoResponse.class)) }),
         @ApiResponse(responseCode = "400", description = "Dano já computado",
             content = @Content),
         @ApiResponse(responseCode = "404", description = "batalha/tipo de ação nao encontrado",
             content = @Content) })
     @PatchMapping("/{uuid}")
-    public ResponseEntity<BatalhaDetalhesResponse> calcularValorTotalDefesaPersonagens(@PathVariable UUID uuid) {
+    public ResponseEntity<DadosTurnoResponse> calcularValorTotalDefesaPersonagens(@PathVariable UUID uuid) {
         return ResponseEntity.ok(combateService.calcularValorTotalAcaoPersonagens(uuid, TipoAcao.DEFESA));
     }
 }
