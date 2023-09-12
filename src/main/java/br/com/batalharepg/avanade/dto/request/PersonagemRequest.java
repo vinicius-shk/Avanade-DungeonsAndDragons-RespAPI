6f07c6807d5e2d57bb2f715ad79c69c5438e8070
vinicius.shk <vinicius.shk@gmail.com>
2023-09-12 11:44:10 -0300

    refactor(Personagem): DtoRequest
    Corrige quebra de linha para manter padrao do projeto

--- src/main/java/br/com/batalharepg/avanade/dto/request/PersonagemRequest.java
+++ src/main/java/br/com/batalharepg/avanade/dto/request/PersonagemRequest.java
@@ -4,7 +4,9 @@ import br.com.batalharepg.avanade.factory.personagem.TipoClassePersonagem;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record PersonagemRequest(@NotNull(message = "Nome deve ser informado") @Size(min = 2, max = 20, message = "Nome deve ter entre 2 e 20 caracteres") String nome,
+public record PersonagemRequest(@NotNull(message = "Nome deve ser informado")
+                                @Size(min = 2, max = 20, message = "Nome deve ter entre 2 e 20 caracteres")
+                                String nome,
                                 @NotNull(message = "Tipo de classe deve ser informado")
                                 TipoClassePersonagem tipoClassePersonagem) {
 }
